ea0bddc23fcd55a6a7c95018ead301398a1d4fa1
Ong Shi Yan <osy2003osy@gmail.com>
2026-04-18 10:47:58 +0800

    add createAccController.php

---
+++ control/createAccController.php
@@ -0,0 +1,11 @@
+<?php
+	require_once __DIR__ . '/../config/DBConnection.php';
+	require_once __DIR__ . '/../entity/UserAdmin.php';
+	class createAccController {
+		 public function create(string $name, string $username, string $email, string $phone, string $password, string $profile): bool {
+			 $db = DBConnection::getInstance();
+			 $ua = new UserAdmin($db);
+			 return $ua->createUser($name, $username, $email, $phone, $password, $profile);
+		 }
+	}
+?>
