8cb735cd26141d260194c448c52e39d6bccc12c0
Ong Shi Yan <osy2003osy@gmail.com>
2026-04-18 11:12:31 +0800

    added createAccPage.php

---
+++ boundary/createAccPage.php
@@ -0,0 +1,30 @@
+<?php
+	require_once __DIR__ . '/../control/createAccController.php';
+	
+	$controller = new createAccController();
+	
+	$message = "";
+	$msgClass = "";
+	
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+			$name = $_POST['name'];
+			$username = $_POST['username'];
+			$email = $_POST['email'];
+			$phone = $_POST['phone'];
+			$password = $_POST['password'];
+			$profile = $_POST['profile'];
+			
+		$success = $controller->create($name, $username, $email, $phone, $password, $profile);
+
+		if ($success) {
+			$message = "Account created successfully!";
+			$msgClass = "success";
+		}
+		else {
+			$message = "Username already exists.";
+			$msgClass = "error";
+		}
+	}
+	
+	require_once __DIR__ . '/views/create_account.view.php';
+?>
